dd9686df56645905e1b05a95292426de43a669f5
mturilin <mturilin@gmail.com>
2022-12-18 14:20:33 -0600

    Changed GitHub link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -72,7 +72,7 @@ const config = {
           },
           { to: '/blog', label: 'Blog', position: 'left' },
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/gojocash/stablecoin-guides',
             label: 'GitHub',
             position: 'right',
           },
